7ad4d51b567c9f43cd6434ec847f82192f431884
Tyler Garlick <tjgarlick@gmail.com>
2013-05-04 17:29:42 -0400

    Introduced IRavenRepository

---
+++ RedRocket.Persistence.RavenDb/RedRocket.Persistence.RavenDb/IRavenRepository.cs
@@ -0,0 +1,9 @@
+﻿using RedRocket.Persistence.Common;
+
+namespace RedRocket.Persistence.RavenDb
+{
+    public interface IRavenRepository<T> : IRepository<T> where T : class
+    {
+        T FindWithKey(string id);
+    }
+}
--- RedRocket.Persistence.RavenDb/RedRocket.Persistence.RavenDb/Repository.cs
+++ RedRocket.Persistence.RavenDb/RedRocket.Persistence.RavenDb/Repository.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using Raven.Client;
-using RedRocket.Persistence.Common;
 using RedRocket.Utilities.Core.Validation;
 
 namespace RedRocket.Persistence.RavenDb
 {
-    public class RavenRepository<T> : IRepository<T> where T : class
+    public class RavenRepository<T> : IRavenRepository<T> where T : class
     {
         public RavenRepository(ICurrentDocumentSession currentDocumentSession)
         {
