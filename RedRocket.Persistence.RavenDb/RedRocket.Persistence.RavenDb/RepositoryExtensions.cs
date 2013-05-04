4a952a60e6840a8082a7a82f48956673706e0802
Tyler Garlick <tjgarlick@gmail.com>
2013-05-04 17:53:24 -0400

    fixed session issue on Repository Extensions

---
+++ RedRocket.Persistence.RavenDb/RedRocket.Persistence.RavenDb/RepositoryExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using RedRocket.Persistence.Common;
+
+namespace RedRocket.Persistence.RavenDb
+{
+    public static class RepositoryExtensions
+    {
+        public static T FindWithKey<T>(this IRepository<T> repository,  string id) where T : class
+        {
+            var ravenRepository = repository as RavenRepository<T>;
+            if(ravenRepository == null)
+                throw new NullReferenceException("Raven Repository was not properly setup.  Please check your wireup settings and try again.");
+            
+            return ravenRepository.Session.Load<T>(id);
+        }
+    }
+}
